af37f59a282ff63cc6e8588ed24ee3a788a0e9b2
aka <benmo0802@gmail.com>
2025-07-23 17:32:18 +0800

    feat: remove @nuxt/ui dependency and integrate UI components with theme toggle

--- src/app.tsx
+++ src/app.tsx
@@ -1,25 +1,42 @@
 import { RouterLink, RouterView } from 'vue-router'
 import HelloWorld from './components/hello-world'
 import { defineComponent } from 'vue'
+import UApp from '@nuxt/ui/components/App.vue'
+import UButton from '@nuxt/ui/components/Button.vue'
+
+import { useColorMode } from '@vueuse/core'
 
 export default defineComponent({
   name: 'App',
   setup() {
+    const mode = useColorMode()
+
     return () => (
-      <div>
-        <header>
-          <div class="wrapper">
-            <HelloWorld msg="You did it!" />
+      <UApp>
+        <div>
+          <header>
+            <div class="wrapper">
+              <HelloWorld msg="You did it!" />
 
-            <nav>
-              <RouterLink to="/">Home</RouterLink>
-              <RouterLink to="/about">About</RouterLink>
-            </nav>
-          </div>
-        </header>
+              <nav>
+                <RouterLink to="/">Home</RouterLink>
+                <RouterLink to="/about">About</RouterLink>
+              </nav>
+            </div>
+          </header>
 
-        <RouterView />
-      </div>
+          <RouterView />
+
+          <UButton
+            icon={mode.value === 'dark' ? 'i-lucide-moon' : 'i-lucide-sun'}
+            color="neutral"
+            variant="ghost"
+            onClick={() => {
+              mode.value = mode.value === 'dark' ? 'light' : 'dark'
+            }}
+          />
+        </div>
+      </UApp>
     )
   },
 })
--- src/views/home.tsx
+++ src/views/home.tsx
@@ -1,14 +1,19 @@
 import { defineComponent } from 'vue'
+import UButton from '@nuxt/ui/components/Button.vue'
+
 export default defineComponent({
   name: 'HomeView',
   setup() {
-    return () => (
-      <main>
-        <div>
-          <h1 class={'text-red-500'}>Welcome to the Home Page</h1>
-          <p>This is the home page of the application.</p>
-        </div>
-      </main>
-    )
+    return () => view
   },
 })
+
+const view = (
+  <main>
+    <div>
+      <h1 class={'text-red-500'}>Welcome to the Home Page</h1>
+      <p>This is the home page of the application.</p>
+      <UButton variant='soft' onClick={() => console.log('hello')}>Click Me</UButton>
+    </div>
+  </main>
+)
